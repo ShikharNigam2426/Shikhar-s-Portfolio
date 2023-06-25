9495bb61687eaeb944741399aad0256121f334f3
ShikharNigam2426 <122465304+ShikharNigam2426@users.noreply.github.com>
2023-06-25 09:19:27 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const screenSizeQuery = window.matchMedia('(max-width: 768px)');
+const paragraphs = document.querySelectorAll('p.card-text');
+const buttons = document.querySelectorAll('.btn');
+
+// p tag ki html remove krne ki js 
+const removeCardTextContent = () => {
+
+    paragraphs.forEach((paragraph) => {
+        paragraph.innerHTML = '';
+    });
+};
+
+
+// btn ka FW change krne ka js 
+const buttonFontSize300 = () => {
+    buttons.forEach((button) => {
+        button.style.fontWeight = '500';
+        button.style.width = '100%';
+        button.innerHTML = `Click `;
+    });
+};
+
+// btn ks FW change krne ka js 
+
+// p tag remove krne ka js 
+const handleScreenSizeChange = (query) => {
+    if (query.matches) {
+        removeCardTextContent();
+        buttonFontSize300();
+    }
+    else{
+        buttons.forEach = () => {
+           button.style.fontWeight = 'normal';   
+        } 
+    }
+};
+
+handleScreenSizeChange(screenSizeQuery); // Run on initial page load
+
+screenSizeQuery.addListener(handleScreenSizeChange); // Listen for screen size changes
